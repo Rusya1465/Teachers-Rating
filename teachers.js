cae3c3914dc4466d08e0b32f2da4cd0fa0bf1122
Finding-Paradise <62791073+Finding-Paradise@users.noreply.github.com>
2023-05-04 12:38:08 +0600

    added more teachers

--- teachers.js
+++ teachers.js
@@ -2,6 +2,10 @@ import photo_1 from './src/assets/teachers/ermek.jpeg'
 import photo_2 from './src/assets/teachers/guljainar.jpeg'
 import photo_3 from './src/assets/teachers/irshad.jpeg'
 import photo_4 from './src/assets/teachers/nurmuhammed.jpeg'
+import photo_5 from './src/assets/teachers/artur.jpeg'
+import photo_6 from './src/assets/teachers/ruslan.jpeg'
+import photo_7 from './src/assets/teachers/nazima.jpeg'
+import photo_8 from './src/assets/teachers/murray.jpeg'
 
 const teachers = [
     {
@@ -28,7 +32,30 @@ const teachers = [
       photoUrl: photo_3,
       rating: 5
     },
-    
+    {
+      id:5,
+      name:'Artur',
+      photoUrl: photo_5,
+      rating: 5
+    },
+    {
+      id:6,
+      name:'Ruslan',
+      photoUrl: photo_6,
+      rating: 5
+    },
+    {
+      id:7,
+      name:'Nazima',
+      photoUrl: photo_7,
+      rating: 5
+    },
+    {
+      id:8,
+      name:'Murray',
+      photoUrl: photo_8,
+      rating: 5
+    },
   ]
 
 export default teachers
