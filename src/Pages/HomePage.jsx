c91516b9920914b9b046126934b9e452f041e3e1
Finding-Paradise <janarplanet.kg@gmail.com>
2023-05-20 17:13:11 +0600

    added button on HomePage

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -3,13 +3,16 @@ import '../styles/HomePage.css'
 import wback_1 from '../assets/welcomeBack-1.svg'
 import wback_2 from '../assets/welcomeBack-2.svg'
 import wback_3 from '../assets/welcomeBack-3.svg'
+import { useNavigate } from 'react-router-dom';
 
 
 function HomePage() {
+    const navigate = useNavigate()
     return (
         <>
             <main className="main">
                 <h1 className="main--heading">Rate My Teachers</h1>
+                <button className='main--rate-btn' onClick={() => {navigate('/teachers')}}>Start Rating</button>
             </main>
             <div className="welcomeBack">
                 <h1 className="welcomeBack--heading">
